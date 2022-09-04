cf708b71ad320125da35bfa48e2d72d2585756fc
shadowino <savlovichdenis@gmail.com>
2022-09-04 22:57:34 +0300

    Arduino firmware update
    add: ds1302 библиотека
    add: compareTime() ищет текущее время в расписании
    mod: httpReqwest больше не отправляет сообщений в Serial

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -11,9 +11,9 @@
 
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
-
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <DS1302.h>
 
 #ifndef STASSID
 #define STASSID "tp3020"
@@ -21,21 +21,24 @@
 #endif
 
 //setting
-#define DEBUGMODE true //DEBUG Mode, swith to true to enable Serial debug info !!Warning!! many massages!
+#define DEBUGMODE false //DEBUG Mode, swith to true to enable Serial debug info !!Warning!! many massages!
 
 // порт подключения реле
 #define pin_relay 02
+#define TIMEOUTCALL 30000 // froze time between calling
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
 char json[1024] = {}; // массив символов ???нужен???
-
 const char* host = "http://10.0.1.26/api/today"; // uri для запросов API расписания
+int answerCode = 0;
 
 WiFiClient client;
 HTTPClient http; // обьект http подключения wifi
 DynamicJsonDocument payload(1024);
+DS1302 rtc(5, 2, 4);
+Time currTime;
 
 /*
   запрос к серверу и запись полученного ответа от него
@@ -44,66 +47,69 @@ DynamicJsonDocument payload(1024);
   возвращает код ответа (200|404|501)
 */
 int httpReqwest() {
-  if (DEBUGMODE) {                        // debug mode
-    Serial.print("connecting to ");
-    Serial.println(host);
-    Serial.print("[HTTP] begin...\n");
-  }
-
   if (!http.begin(client, host)) {                // HTTP запрос в network
-    Serial.printf("[HTTP} Unable to connect %s\n", host);
     http.end();
-    Serial.print("closing connection\n");
     client.stop(); // ???
     return 0;
   }
 
-  if (DEBUGMODE) Serial.print("[HTTP] GET...\n");
   int httpCode = http.GET();                    // $httpCode is status qwestion (200 is OK 404 is not found)
 
   if (!(httpCode > 0)) {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     http.end();
-    Serial.print("closing connection\n");
     client.stop(); // ???
     return httpCode;
   }
 
-  if (DEBUGMODE) Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
   if (!(httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)) // file not found at server
   {
-    Serial.printf("[HTTP] GET... complete, error answer: %d\n", httpCode);
     http.end();
-    Serial.print("closing connection\n");
     client.stop(); // ???
     return httpCode;
   }
 
   http.getString().toCharArray(json, 1024);             // http answer to char array
-  //Serial.printf("http data:", json);                // answer from server
-  deserializeJson(payload, json);                   // write string answer to json object
+  deserializeJson(payload, json);                       // write string answer to json object
 
-  if (DEBUGMODE)                            // в режиме DEBUG show answer with json
+  http.end();
+  client.stop(); // ???
+  return httpCode;
+}
+
+
+/*
+  compareTime() сравнивает текущее время с расписанием
+
+  возвращает истинну если время найденно
+*/
+bool compareTime() {
+  currTime = rtc.getTime();
+  //String currTimeStr = currTime.hour + ":" + currTime.min; // время в МК в формате "чч:мм"
+  String currTimeStr = "04:59"; // время в МК в формате "чч:мм"
+  String compTime = "";
+  Serial.print("current time:");
+  Serial.println(currTimeStr);
+
+  if (payload["1"][0].isNull()) return false;
+  for (int d = 1; d < 5; d++) // поиск в расписании текущего времени (+-diff в минутах)
   {
-    Serial.print("getting data:\n");
-    for (int d = 1; d < 5; d++) {
-      // Serial.print(payload[String(d)][0].as<String>());
-      Serial.print(d);
-      Serial.print(":\n");
-      for (int t = 0; t < 4; t++) {
-        Serial.print("-");
-        Serial.println(payload[String(d)][t].as<String>());
+    for (int t = 0; t < 4; t++) {
+      // d - номер пары в str
+      // t - номер звонка в int
+      if (payload[String(d)][t].isNull()) continue;
+      compTime = payload[String(d)][t].as<String>();
+      Serial.print("comare time:");
+      Serial.println(compTime);
+      if (compTime == currTimeStr) {
+        Serial.println("Found! Send Signal!!!");
+        return true;
       }
     }
   }
 
-  http.end();
-  Serial.print("closing connection\n");
-  client.stop(); // ???
-  delay(5000);
-  return httpCode;
+  return false;
 }
+
 
 void setup() {
   Serial.begin(115200);
@@ -129,10 +135,10 @@ void setup() {
 }
 
 void loop() {
-  if (httpReqwest() == 200) {
+  answerCode = httpReqwest();
+  if (answerCode == 200) {
     Serial.print("getting data:\n");
     for (int d = 1; d < 5; d++) {
-      //          Serial.print(payload[String(d)][0].as<String>());
       Serial.print(d);
       Serial.print(":\n");
       for (int t = 0; t < 4; t++) {
@@ -140,9 +146,16 @@ void loop() {
         Serial.println(payload[String(d)][t].as<String>());
       }
     }
+  } else {
+    Serial.print("http reqwest return incorrect answer! code:");
+    Serial.println(answerCode);
   }
+  if (compareTime()){
+    Serial.println("Time detect in loop! Call!");
+    delay(TIMEOUTCALL);
+  }
+
   Serial.println();
   Serial.println("closing connection");
-  client.stop();
   delay(5000);
 }
